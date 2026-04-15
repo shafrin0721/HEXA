cac0be6c675a7c5d7467a69ae7f9844f27c0c4c8
Shafrin Shihan <shafrin.shihan7@gmail.com>
2026-04-16 00:19:45 +0530

    Delete HEXA directory

--- HEXA/server/routes/cartRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getCartItems } from '../controllers/cartController.js';
-
-const router = Router();
-
-router.get('/', getCartItems);
-
-export default router;
--- HEXA/server/routes/productRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getAllProducts, getProductById } from '../controllers/productController.js';
-
-const router = Router();
-
-router.get('/', getAllProducts);
-router.get('/:id', getProductById);
-
-export default router;
