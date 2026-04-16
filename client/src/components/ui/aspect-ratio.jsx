43505722be6657983fe860f57432bd7283c910ce
Shafrin Shihan <shafrin.shihan7@gmail.com>
2026-04-16 11:56:02 +0530

    Merge feature/shafrin-products-cart-order-summary into qa-main-test This pull request merges the feature branch `feature/shafrin-products-cart-order-summary` into `qa-main-test` as requested.

--- client/src/components/ui/aspect-ratio.jsx
+++
@@ -1,9 +0,0 @@
-import * as AspectRatioPrimitive from "@radix-ui/react-aspect-ratio";
-const AspectRatio = AspectRatioPrimitive.Root;
-export {
-  AspectRatio
-<<<<<<< HEAD
-};
-=======
-};
->>>>>>> 3c302825c6dff40f9e879d68d931ce8fa9e451b5
