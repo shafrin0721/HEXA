5e4dae7ecf26590e3eb0d3555659ddaa589173d1
Nashath Sara <nashathsara05@gmail.com>
2026-04-16 09:51:35 +0530

    Deleted unwanted folders

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const orderRoutes = require('./routes/orderRoutes');
+const ordersRoutes = require('./routes/orders');
 const paymentsRoutes = require('./routes/payments');
 const authRoutes = require('./routes/authRoutes');
 const contactRoutes = require('./routes/contactRoutes');
 const profileRoutes = require('./routes/profileRoutes');
+const cartRoutes = require('./routes/cart')
 
 dotenv.config();
 const app = express();
@@ -16,9 +17,10 @@ app.use(express.json());
 
 // Routes
 app.use('/api/auth', authRoutes);
+app.use('/api/cart', cartRoutes);
 
 // Routes - IMPORTANT: Order matters
-app.use('/api/orders', orderRoutes);
+app.use('/api/orders', ordersRoutes);
 app.use('/api', paymentsRoutes);
 app.use("/api/contact", contactRoutes);
 app.use("/api/profile", profileRoutes);
@@ -38,4 +40,4 @@ const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
   console.log(`📝 API available at http://localhost:${PORT}/api`);
-});
+});;
